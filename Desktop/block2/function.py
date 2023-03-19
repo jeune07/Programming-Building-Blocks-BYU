9a0602bb5febf60c0306993e71a95afe64f6b66e
jeune  winsley <63593398+jeune07@users.noreply.github.com>
2023-03-19 18:12:10 -0300

    Delete directory

--- Desktop/block2/function.py
+++
@@ -1,9 +0,0 @@
-def wordsprocesing(words):
-    regular_case = words
-    upper_case =words.upper()
-    lower_case=words.lower()
-    print(regular_case,'\n',upper_case,'\n',lower_case)
-    #return regular_case, upper_case,upper_case
-   
-wordsprocesing("winsley the best")
-
--- Desktop/block2/open_file.py
+++
@@ -1,5 +0,0 @@
-with open("C:/Users/Mon Pepe/Desktop/books.txt") as books:
-    for book in books:
-       # print(book)
-        newBook=book.strip()
-        print(newBook)
